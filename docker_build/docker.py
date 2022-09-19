7baed607f2e96d5874eb3114c7d6152e11e52e0f
Peter McDonald <me@petermcdonald.co.uk>
2022-09-19 20:58:05 +0100

    Corrected CICD and refactored

--- docker_build/docker.py
+++ docker_build/docker.py
@@ -1,34 +1,15 @@
 """Module to handle building docker containers."""
 import subprocess
-from dataclasses import dataclass
 from os.path import split
+from pathlib import Path
 from subprocess import PIPE
 
-BINARY_DIRECTORY = 'copied_files/binaries/'
-CONFIG_DIRECTORY = 'copied_files/config/'
+from docker_build.dockerfile import Dockerfile
+from docker_build.exceptions import DockerException
+from docker_build.models import ExposedPortDetails, FileDetails, Response
 
-
-@dataclass
-class FileDetails:
-    """Dataclass for the details of files to copy from a container."""
-
-    filename: str
-    path: str
-    symlink_filename: str
-    symlink_path: str
-
-
-@dataclass
-class Response(object):
-    """Data class to store command responses."""
-
-    code: int
-    stderr: str
-    stdout: str
-
-
-class DockerException(Exception):
-    """Standard Docker exception."""
+BINARY_DIRECTORY = 'binaries/'
+CONFIG_DIRECTORY = 'config/'
 
 
 class Docker(object):
@@ -36,94 +17,75 @@ class Docker(object):
 
     __slots__ = (
         '_base_image',
-        '_commands',
+        '_base_path',
         '_container_id',
-        '_copied_binaries',
-        '_copied_config',
-        '_exposed_ports',
+        '_dockerfile',
     )
 
-    def __init__(self, container_id: str, base_image: str):
+    def __init__(self, base_path: Path, container_id: str, base_image: str):
         """
         Initialise Docker.
 
         Args:
+            base_path: Path to save files too
             container_id: The container ID to work from
             base_image: The image we will build onto
         """
-        self._container_id = container_id
-        self._base_image = base_image
-        self._commands: list[str] = []
-        self._copied_binaries: dict[str, FileDetails] = {}
-        self._copied_config: dict[str, FileDetails] = {}
-        self._exposed_ports: list[str] = []
+        self._dockerfile: Dockerfile = Dockerfile(base_image=base_image)
+        self._base_path: Path = base_path
+        self._container_id: str = container_id
 
-    def run(self, config_files: list[str], binaries: list[str], exposed_ports: list[str], commands: list[str]):
+        binary_directory: Path = Path(base_path, BINARY_DIRECTORY)
+        config_directory: Path = Path(base_path, CONFIG_DIRECTORY)
+
+        binary_directory.mkdir(parents=True, exist_ok=True)
+        config_directory.mkdir(parents=True, exist_ok=True)
+
+    def run(
+            self,
+            config_files: list[str],
+            binaries: list[str],
+            exposed_ports: list[ExposedPortDetails],
+            commands: list[str]
+    ):
         """
         Build a dockerfile based on the inputs.
 
         Args:
             config_files: List of config files to copy out of the origin container
             binaries: List of binaries files to copy out of the origin container
-            exposed_ports: Ports to be be exposed
-            commands: List of commands to be ran during making of the container
+            exposed_ports: Ports to be exposed
+            commands: List of commands to be run during making of the container
         """
-        self._exposed_ports = exposed_ports
-        self._commands = commands
+        self._dockerfile.add_commands(commands=commands)
+        self._dockerfile.add_exposed_ports(exposed_ports=exposed_ports)
+
         self._process_binaries(binaries=binaries)
         self._process_config_files(config_files=config_files)
-        self._build_dockerfile()
 
-    def _build_dockerfile(self):
-        """Build a Dockerfile from the retrieved information."""
-        output = f'FROM {self._base_image}\n\n'
+        self._dockerfile.build(save_path=self._base_path)
 
-        for binary_key in self._copied_binaries.keys():
-            binary_file_details = self._copied_binaries[binary_key]
-            output += (
-                f'COPY {BINARY_DIRECTORY}{binary_file_details.filename} ',
-                f'{binary_file_details.path}/{binary_file_details.filename}\n'
-            )
-
-        output += '\n'
-
-        for config_key in self._copied_config.keys():
-            config_file_details = self._copied_config[config_key]
-            output += (
-                f'COPY {CONFIG_DIRECTORY}{config_file_details.filename} ',
-                f'{config_file_details.path}/{config_file_details.filename}\n'
-            )
-
-        output += '\n'
-
-        for command in self._commands:
-            output += f'RUN {command}\n'
-
-        output += '\n'
-
-        for exposed_port in self._exposed_ports:
-            output += f'EXPOSE {exposed_port}\n'
-
-        with open('Dockerfile', 'w') as fh:
-            fh.write(output)
-
-    def _copy_file(self, file_details: FileDetails, save_folder: str):
+    def _copy_file(self, file_details: FileDetails):
         """
         Copy file from the container.
 
         Args:
             file_details: Details about the file to be copied
         """
-        file_path: str = f'{file_details.path}/{file_details.filename}'
+        if self._dockerfile.file_exists(file=file_details):
+            return
+
+        source_path: str = f'{file_details.path}/{file_details.filename}'
         if file_details.symlink_path:
-            file_path = f'{file_details.symlink_path}/{file_details.symlink_filename}'
-        command = [
+            source_path = f'{file_details.symlink_path}/{file_details.symlink_filename}'
+        command: list[str] = [
             'docker',
             'cp',
-            f'{self._container_id}:{file_path}',
-            f'./{save_folder}/{file_details.filename}',
+            f'{self._container_id}:{source_path}',
+            f'{file_details.saved_path}',
         ]
         self._run_command(command=command)
+        self._dockerfile.add_files(files=file_details)
 
     def _get_file_origin(self, filename: str) -> FileDetails:
         """
@@ -135,7 +97,7 @@ class Docker(object):
         Returns:
             Real path and filename
         """
-        ls_command = [
+        ls_command: list[str] = [
             'docker',
             'exec',
             self._container_id,
@@ -152,13 +114,15 @@ class Docker(object):
         binary_details = FileDetails(
             filename=filename,
             path=path,
+            saved_path=Path(),
+            saved_path_relative=Path(),
             symlink_filename='',
             symlink_path='',
         )
         if binary_ls_split[-2] != '->':
             return binary_details
         symlink_filename = split(binary_ls_split[-1])[-1]
-        find_command = [
+        find_command: list[str] = [
             'docker',
             'exec',
             self._container_id,
@@ -179,7 +143,6 @@ class Docker(object):
                         find_stdout_line.startswith('/usr/lib/'):
                     find_path = find_stdout_line
                     break
-
         else:
             find_path = find_stdout_lines[0]
         binary_details.symlink_path = split(find_path)[0]
@@ -211,11 +174,11 @@ class Docker(object):
                 file_details = self._get_file_origin(
                     filename=file_ldd_path
                 )
+                file_details.saved_path = Path(self._base_path, BINARY_DIRECTORY, file_details.filename)
+                file_details.saved_path_relative = Path('./', BINARY_DIRECTORY, file_details.filename)
             except FileNotFoundError:
                 continue
-            full_path = f'{file_details.path}/{file_details.filename}'
-            self._copy_file(file_details=file_details, save_folder=BINARY_DIRECTORY)
-            self._copied_binaries[full_path] = file_details
+            self._copy_file(file_details=file_details)
 
     def _process_binaries(self, binaries: list[str]):
         """
@@ -225,7 +188,7 @@ class Docker(object):
             binaries: List of binary files to process
         """
         for binary in binaries:
-            command = [
+            command: list[str] = [
                 'docker',
                 'exec',
                 self._container_id,
@@ -237,10 +200,9 @@ class Docker(object):
             file_details = self._get_file_origin(
                 filename=binary
             )
-            full_path = f'{file_details.path}/{file_details.filename}'
-            if full_path not in self._copied_binaries:
-                self._copy_file(file_details=file_details, save_folder=BINARY_DIRECTORY)
-                self._copied_binaries[full_path] = file_details
+            file_details.saved_path = Path(self._base_path, BINARY_DIRECTORY, file_details.filename)
+            file_details.saved_path_relative = Path('./', BINARY_DIRECTORY, file_details.filename)
+            self._copy_file(file_details=file_details)
 
     def _process_config_files(self, config_files: list[str]):
         """
@@ -250,14 +212,12 @@ class Docker(object):
             config_files: List of config files to process
         """
         for config_file in config_files:
-
             file_details = self._get_file_origin(
                 filename=config_file
             )
-            full_path = f'{file_details.path}/{file_details.filename}'
-            if full_path not in self._copied_config:
-                self._copy_file(file_details=file_details, save_folder=CONFIG_DIRECTORY)
-                self._copied_config[full_path] = file_details
+            file_details.saved_path = Path(self._base_path, CONFIG_DIRECTORY, file_details.filename)
+            file_details.saved_path_relative = Path('./', CONFIG_DIRECTORY, file_details.filename)
+            self._copy_file(file_details=file_details)
 
     @staticmethod
     def _run_command(command: list[str]) -> Response:
---
+++ tests/test_dummy.py
@@ -0,0 +1,6 @@
+"""Dummy test to satisfy pytest until real tests are written."""
+
+
+def test_dummy():
+    """Dummy test."""
+    assert 1 == 1
